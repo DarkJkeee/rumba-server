9281ff8079bada7ce48013a375d83255a35a113b
burstein <burstein@yandex-team.ru>
2021-12-29 22:44:52 +0200

    First commit

---
+++ src/main/java/com/company/rumba/RumbaApplication.java
@@ -0,0 +1,13 @@
+package com.company.rumba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumbaApplication.class, args);
+	}
+
+}
